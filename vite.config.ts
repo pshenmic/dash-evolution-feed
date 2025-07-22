660f84ff87f1a4d3c38ff86dabb78f7d777c4723
pshenmic <pshenmic@gmail.com>
2025-07-23 03:31:02 +0700

    Remove all deoptimizations

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  mode: "development",
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  build: {
-    sourcemap: false,
-    terserOptions: {
-      keep_classnames: true,
-      keep_fnames: true
-    },
-    minify: false
-  },
-  esbuild: {
-    minifyIdentifiers: false,
-    minifySyntax: false,
-    keepNames: true,
-    treeShaking: false
-  },
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
